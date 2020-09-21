c2723180842d5e18806e73cd0c574f77882e5154
JMundondo <jephiastmundondo@gmail.com>
2020-09-21 16:08:18 +0200

    register_ui

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Providers\RouteServiceProvider;
+
+use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use App\Student;
+use App\Teacher;
+
+
+use Illuminate\Http\Request;
+
+class RegisterController extends Controller
+{   protected function index()
+    {
+        return view('auth.register');
+        
+    }
+    protected function validator(array $data)
+
+    {
+        return Validator::make($data, [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
+        ]);
+    }
+
+    /**
+     * Create a new user instance after a valid registration.
+     *
+     * @param  array  $data
+     * @return \App\User
+     */
+    
+    protected function createTeacher(Request $request)
+    {
+        
+        
+        return Teacher::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+        $methods = Teacher::all();
+           dd($methods);
+    }
+    /*protected function createTeacher(array $data)
+    {
+        return Teacher::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+        ]);
+    }*/
+}
+
+
+
---
+++ app/Http/Controllers/UserRegisterController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+namespace App\Http\Controllers;
+use App\Providers\RouteServiceProvider;
+
+use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use App\Student;
+
+use Illuminate\Http\Request;
+
+class UserRegisterController extends Controller
+{
+    //register new stundent users 
+    protected function validator(array $data)
+
+    {
+        return Validator::make($data, [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
+        ]);
+    }
+
+    /**
+     * Create a new user instance after a valid registration.
+     *
+     * @param  array  $data
+     * @return \App\User
+     */
+    protected function createStudent(array $data)
+    {
+        return Student::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+        ]);
+    }
+}
